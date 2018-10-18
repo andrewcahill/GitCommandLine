00799ae04a55e106ca83006ab2f37a7d4146f5f6
John Doe <john@johndoe.com>
2018-10-18 20:45:31 +0400

    Updated output

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitCommandLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
